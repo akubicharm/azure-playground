37f74f162362d4ff9b66506b6be737617cf4d9a7
Kei Omizo <kay.akubichan@gmail.com>
2018-07-06 15:25:11 +0900

    update FILE variasbles

--- azure-iot-edge/ubuntu/setup.sh
+++ azure-iot-edge/ubuntu/setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 DEVICEID=$1
-FILE=config.yaml
+FILE=/etc/iotedge/config.yaml
 
 perl -pi -e "s/^  device_connection_string: \"\"/  device_connection_string: \"$DEVICEID\"/g" $FILE
 
 exit
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install moby-engine
-sudo apt-get install moby-cli 
+apt-get install moby-engine
+apt-get install moby-cli 
 
-sudo apt-get update
-sudo apt-get install iotedge
+apt-get update
+apt-get install iotedge
 
 
 # edit /etc/iotedge/config.yaml
 
 
 
-sudo systemctl restart iotedge
-sudo systemctl status iotedge
+systemctl restart iotedge
+systemctl status iotedge
